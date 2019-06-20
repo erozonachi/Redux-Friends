074d8d1666d477207e4c8384b3858ba7791535e6
erozonachi <erozonachi.james@gmail.com>
2019-06-20 15:12:07 +0100

    Create and export action types

---
+++ friends/src/actions/index.js
@@ -0,0 +1,6 @@
+export const FETCHING = `FETCHING`;
+export const SUCCESS = `SUCCESS`;
+export const FAILURE = `FAILURE`;
+export const SAVING = `SAVING`;
+export const UPDATING = `UPDATING`;
+export const DELETING = `DELETING`;
